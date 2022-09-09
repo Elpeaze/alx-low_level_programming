f771834966d49b5efb6dd90bffa69751d756a718
Elpeaze <elpeaze@gmail.com>
2022-09-08 18:25:30 -0700

    My project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - print a text
+ *
+ * return: Always 0 (success)
+ * Description:main - prints out a string to stdout.
+ *  return:0 if success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
