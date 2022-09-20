450e9e5c460b156504890318e7a89f54c6a17776
Elpeaze <elpeaze@gmail.com>
2022-09-20 08:18:08 -0700

    My project on pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers,
+ * followed by a new line
+ * @n: number of elements of an array
+ */
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+		if (index != n - 1)
+			printf(", ");
+	}
+	putchar('\n');
+}
