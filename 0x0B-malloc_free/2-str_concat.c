720ea14c2e3ed5d6b08709baa918432ad179aab7
Elpeaze <elpeaze@gmail.com>
2022-10-04 18:52:15 -0700

    Malloc free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -37,13 +37,13 @@ char *str_concat(char *s1, char *s2)
 	if (s2)
 	{
 		while (i < (len1 + len2))
-				{
-				s3[i] = s2[j];
-				i++;
-				j++;
-				}
-				}
-				s3[i] = '\0';
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+	s3[i] = '\0';
 
-				return (s3);
-				}
+	return (s3);
+}
