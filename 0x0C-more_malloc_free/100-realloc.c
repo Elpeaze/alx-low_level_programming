0a99dcd2e1db9cdeb654a64c4ecfe1db3b0b126c
Elpeaze <elpeaze@gmail.com>
2022-10-06 13:00:27 -0700

    more malloc,free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,29 +1,51 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-/**
- * array_range - creates an array of integers
- *
- * @min: values from included
- * @max: from this
- *
- * Return: pointer to allocated memory
- */
-int *array_range(int min, int max)
-{
-	int *m;
-	int i;
-	int x = min;
 
-	if (min > max)
-		return (NULL);
-	m = malloc(sizeof(int) * ((max + 1) - (min)));
-	if (m == NULL)
-		return (NULL);
-	for (i = 0; x <= max; i++)
+/**
+ * _realloc - multiplies two digits
+ * @ptr: 1st digit in the multiplication
+ * @old_size: 1st digit in the multiplication
+ * @new_size: 1st digit in the multiplication
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i;
+	char *new_ptr;
+	char *ptr2 = ptr;
+
+	if (new_size == 0)
 	{
-		m[i] = x;
-		x++;
+		if (ptr != NULL)
+			free(ptr);
+		ptr = NULL;
+		return (NULL);
 	}
-	return (m);
+
+	if (ptr == NULL)
+	{
+		new_ptr = (malloc(new_size));
+		if (new_ptr == NULL)
+			return (NULL);
+		return (new_ptr);
+	}
+
+	if (new_size == old_size)
+		return (ptr);
+
+	new_ptr = (char *)malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
+
+	/*copy from old_size to new_size*/
+	for (i = 0; i < new_size && i < old_size; i++)
+	{
+		new_ptr[i] = ptr2[i];
+	}
+	free(ptr);
+	ptr = NULL;
+	ptr2 = NULL;
+	return (new_ptr);
 }
