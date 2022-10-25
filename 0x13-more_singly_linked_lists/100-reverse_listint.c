90c427d076a6c56e9849961fc1ebf7ecd71066c1
Elpeaze <elpeaze@gmail.com>
2022-10-25 08:59:12 -0700

    Quick fix

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * reverse_listint - function that reverses a linked list
+ * reverse_listint - Reverses a listint_t list.
  * @head: A pointer to the address of
  *        the head of the list_t list.
  *
